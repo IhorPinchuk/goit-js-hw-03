3cb39f1915c49c432bf717584563f31283745f45
IhorPinchuk <pinchukkk@ukr.net>
2023-01-25 23:52:43 +0200

    task-18

--- js/script-1.js
+++ js/script-1.js
@@ -379,3 +379,56 @@
 
 // ------------------------------task 18------------------------------
 
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//     }
+//     return null;
+//   // Change code above this line
+// }
+
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+
+// ------------------------------task 19------------------------------
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+    const valueFromPropName = [];
+    for (const product of products) {
+        // console.log(product[propName]);
+        if (product[propName] === propName) {
+            
+ valueFromPropName.push(product[propName]);
+        }   
+        // return valueFromPropName;
+    }
+    
+
+  // Change code above this line
+}
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("category"));
